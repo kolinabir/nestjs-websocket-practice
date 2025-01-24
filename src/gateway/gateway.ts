d31cfa7cc661a3fd967860f3c368bbf486a1d242
kolinabir <knkolin9@gmail.com>
2025-01-24 20:46:55 +0600

    implement WebSocket connection handling and emit new messages in MyGateway

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,14 +1,32 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 
+import { Server } from 'socket.io';
+
 @WebSocketGateway({})
-export class MyGateway {
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('COnnected');
+    });
+  }
+
   @SubscribeMessage('newMessage')
   handleNewMessage(@MessageBody() body: any) {
     console.log('New message');
     console.log(body);
+    this.server.emit('newMessage', {
+      message: 'New message',
+      contest: body,
+    });
   }
 }
